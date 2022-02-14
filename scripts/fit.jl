4240c46ef8eafbedd86dbe0607ed7a8f07a6d172
Nikola Janjusevic <nikopjj@gmail.com>
2022-02-13 23:15:47 -0500

    fit bug with type comparison and Flux.cpu

--- scripts/fit.jl
+++ scripts/fit.jl
@@ -3,11 +3,12 @@ using Flux, CUDA
 using FileIO
 
 CUDA.allowscalar(false)
-device = CUDA.functional() ? begin @info "Using GPU."; gpu end : cpu
+device = CUDA.functional() ? begin @info "Using GPU."; Flux.gpu end : identity
 
 # get argument filename and device
 if isinteractive()
-	fn = "scripts/args.yml"
+	# fn = "scripts/args.yml"
+	fn = "args.d/PiBCANet-prgtest-1-prg3.yml"
 else
 	fn = ARGS[1]
 end
@@ -18,11 +19,11 @@ mkpath(args[:train][:savedir])
 
 # instantiate network
 loadingckpt = !isnothing(args[:ckpt]) && isfile(args[:ckpt])
-net = PiBCANet(; args[:net]..., lipschitz_init=!loadingckpt) |> device
+net = PiBCANet(; args[:net]..., lipschitz_init=!loadingckpt) 
 @show net
 
 # instantiate optimizer
-opt = ADAM(args[:opt][:η]) |> device
+opt = ADAM(args[:opt][:η]) 
 
 # load checkpoint
 start = 1
@@ -30,6 +31,9 @@ if loadingckpt
 	@info "Loading checkpoint $(args[:ckpt])..."
 	ckpt = load(args[:ckpt])
 	net′ = ckpt[:net] |> device
+	print("checkpoint network: ")
+	@show net′
+
 	if net′ isa typeof(net)
 		@info "ckpt-net is of same type: continuing training..."
 		net = net′
@@ -38,6 +42,7 @@ if loadingckpt
 	else
 		@assert sum(net′.warps) == sum(net.warps) - 1 "ckpt-net must have 1 fewer BCANet modules than net!"
 		@info "ckpt-net is of different type: loading weights into larger model..."
+
 		d = net.scales - net′.scales
 		for j in 1:net′.scales, w in 1:net′.warps[j]
 			net.netarr[d+j][w] = deepcopy(net′[j][w])
@@ -51,8 +56,7 @@ dstrn = FlyingChairsDataSet(args[:data][:root], augment(;args[:data][:augment]..
 dsval = FlyingChairsDataSet(args[:data][:root]; split=:val, args[:data][:params]...)
 @show dstrn.name, dsval.name
 
-# save updated argument file
-args[:ckpt] = joinpath(args[:train][:savedir], "net.bson")
+# write args file 
 fn = joinpath(args[:train][:savedir], "args.yml")
 saveargs(fn, args)
 
@@ -65,5 +69,9 @@ else
 	@error "Loss function $lossfcn not implemented."
 end
 
-train!(net, (trn=dstrn, val=dsval), opt; start=start, device=device, args[:train]...)
+train!(net |> device, (trn=dstrn, val=dsval), opt |> device; start=start, device=device, args[:train]...)
+
+# save updated argument file
+args[:ckpt] = joinpath(args[:train][:savedir], "net.bson")
+saveargs(fn, args)
 
